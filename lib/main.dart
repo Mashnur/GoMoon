c2f4390fed10fe01817db5f909563ec9ddaf9398
Mashnur <221007712@eastdelta.edu.bd>
2025-07-03 01:22:39 +0600

    add background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_moon/Page/home_page.dart';
 
 void main() {
   runApp(const App());
@@ -14,7 +15,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0),
       ),
-      home: Scaffold(),
+      home: HomePage(),
     ); //
   }
 }
