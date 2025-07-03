e460d9a8c55ccca8bb3b518c7bbe62716e98f41e
Mashnur <221007712@eastdelta.edu.bd>
2025-07-03 13:21:29 +0600

    adding homepage removing unnecessary width method

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHeight, _deviceWidth;
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/astro_moon.png"), // ✅ fixed
-            fit: BoxFit.contain,
-          ),
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [_astroImageWidget(), _pageTitle()],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 70,
+        fontWeight: FontWeight.bold,
+        shadows: [
+          Shadow(blurRadius: 10.0, color: Colors.black87, offset: Offset(2, 2)),
+        ],
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/astro_moon.png"),
+          fit: BoxFit.cover,
         ),
       ),
     );
